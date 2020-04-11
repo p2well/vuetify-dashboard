b41d3ea4683a23d8c85a7ee8399462280249a6f4
p2well <pawel@p2well.com>
2020-04-11 09:14:52 +0200

    Change Hello World to redirect to GitHub repo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,15 +13,17 @@
     <v-row>
       <v-col class="d-inline-flex justify-center">
         <h1 class="display-2">
-          Welcome to Vuetify Dashboard
+          Welcome
         </h1>
       </v-col>
     </v-row>
     <v-row>
       <v-col class="d-inline-flex justify-center">
         <p class="subheading font-weight-regular">
-          For more information about Vuetify please visit
-          <a href="https://vuetifyjs.com" target="_blank">Vuetify Website</a>
+          For more information about this application please see this
+          <a href="https://github.com/p2well/vuetify-dashboard" target="_blank"
+            >GitHub repository</a
+          >
         </p>
       </v-col>
     </v-row>
